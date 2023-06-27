144d3cc70d90bf3c34a3348e7ad603a6531d2cac
Wandati <Petersimon801@gmail.com>
2023-06-27 12:28:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+
+ function SubmitData(Name,Email){
+    const Data ={
+        name: Name,
+        email: Email,
+    }
+    return fetch('http://localhost:3000/users',{
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body:JSON.stringify(Data)
+    })
+    .then((res)=>res.json())
+    .then((data)=>data)
+    .catch((error)=>alert(`${error.message}`))
+
+ }
